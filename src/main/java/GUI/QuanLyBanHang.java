015734f97d2d190924d6d46f9d8e25b26c419616
sittranlong <sittranlong@gmail.com>
2024-03-23 00:39:57 +0700

    Đẩy Giao Diện Quản Lý Bán Hàng Lần 2

--- src/main/java/GUI/QuanLyBanHang.java
+++ src/main/java/GUI/QuanLyBanHang.java
@@ -43,6 +43,14 @@ public class QuanLyBanHang extends javax.swing.JPanel {
         jButtonTimKiemKhachHang = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jButtonThemMoiKhachHang = new javax.swing.JButton();
+        jButtonTreoHoaDon = new javax.swing.JButton();
+        jComboBoxLocKieuDang = new javax.swing.JComboBox<>();
+        jComboBoxLocSize = new javax.swing.JComboBox<>();
+        jComboBoxLocDeGiay = new javax.swing.JComboBox<>();
+        jComboBoxLocGiaSanPham = new javax.swing.JComboBox<>();
+        jLabelTenSanPham = new javax.swing.JLabel();
+        jTextFieldTimTenSanPham = new javax.swing.JTextField();
+        jButtonTimTenSanPham = new javax.swing.JButton();
 
         jLabelQUANLYBANHANG.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabelQUANLYBANHANG.setText("QUẢN LÝ BÁN HÀNG");
@@ -102,6 +110,20 @@ public class QuanLyBanHang extends javax.swing.JPanel {
 
         jButtonThemMoiKhachHang.setText("Thêm Mới");
 
+        jButtonTreoHoaDon.setText("Treo Hóa Đơn");
+
+        jComboBoxLocKieuDang.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jComboBoxLocSize.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jComboBoxLocDeGiay.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jComboBoxLocGiaSanPham.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jLabelTenSanPham.setText("Tìm Tên Sản Phẩm");
+
+        jButtonTimTenSanPham.setText("Tìm");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -131,9 +153,28 @@ public class QuanLyBanHang extends javax.swing.JPanel {
                                         .addComponent(jButtonTimKiemKhachHang, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(jButtonThemMoiKhachHang, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                                 .addComponent(jLabel1))))
-                    .addComponent(jLabelSanPham)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 640, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane3))
+                    .addComponent(jScrollPane3)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addComponent(jLabelSanPham)
+                            .addGap(29, 29, 29)
+                            .addComponent(jLabelTenSanPham)
+                            .addGap(18, 18, 18)
+                            .addComponent(jTextFieldTimTenSanPham, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(jButtonTimTenSanPham)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jComboBoxLocKieuDang, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(29, 29, 29)
+                            .addComponent(jComboBoxLocSize, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(29, 29, 29)
+                            .addComponent(jComboBoxLocDeGiay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(32, 32, 32)
+                            .addComponent(jComboBoxLocGiaSanPham, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 640, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(18, 18, 18)
+                            .addComponent(jButtonTreoHoaDon))))
                 .addGap(0, 469, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -166,12 +207,26 @@ public class QuanLyBanHang extends javax.swing.JPanel {
                 .addGap(29, 29, 29)
                 .addComponent(jLabelGioHang)
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
-                .addComponent(jLabelSanPham)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButtonTreoHoaDon))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 235, Short.MAX_VALUE)
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabelSanPham)
+                        .addGap(18, 18, 18))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jComboBoxLocKieuDang, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jComboBoxLocSize, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jComboBoxLocDeGiay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jComboBoxLocGiaSanPham, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabelTenSanPham)
+                            .addComponent(jTextFieldTimTenSanPham, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButtonTimTenSanPham))
+                        .addGap(29, 29, 29)))
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 247, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -179,6 +234,12 @@ public class QuanLyBanHang extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonThemMoiKhachHang;
     private javax.swing.JButton jButtonTimKiemKhachHang;
+    private javax.swing.JButton jButtonTimTenSanPham;
+    private javax.swing.JButton jButtonTreoHoaDon;
+    private javax.swing.JComboBox<String> jComboBoxLocDeGiay;
+    private javax.swing.JComboBox<String> jComboBoxLocGiaSanPham;
+    private javax.swing.JComboBox<String> jComboBoxLocKieuDang;
+    private javax.swing.JComboBox<String> jComboBoxLocSize;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabelGioHang;
     private javax.swing.JLabel jLabelHoaDonCho;
@@ -186,6 +247,7 @@ public class QuanLyBanHang extends javax.swing.JPanel {
     private javax.swing.JLabel jLabelSDTKhachHang;
     private javax.swing.JLabel jLabelSanPham;
     private javax.swing.JLabel jLabelTenKhachHang;
+    private javax.swing.JLabel jLabelTenSanPham;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
@@ -194,5 +256,6 @@ public class QuanLyBanHang extends javax.swing.JPanel {
     private javax.swing.JTable jTableSanPham;
     private javax.swing.JTextField jTextFieldSDTKhachHang;
     private javax.swing.JTextField jTextFieldTenKhachHang;
+    private javax.swing.JTextField jTextFieldTimTenSanPham;
     // End of variables declaration//GEN-END:variables
 }
