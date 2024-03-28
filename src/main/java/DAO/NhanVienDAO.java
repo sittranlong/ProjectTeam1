3ccf7eb794bdd982e64b3428b848165f7b7efb93
sittranlong <sittranlong@gmail.com>
2024-03-28 17:45:49 +0700

    Update Quản Lý Nhân Viên P1

--- src/main/java/DAO/NhanVienDAO.java
+++ src/main/java/DAO/NhanVienDAO.java
@@ -4,10 +4,122 @@
  */
 package DAO;
 
+import DATABASE.DatabaseHelper;
+import ENTITY.NhanVien;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  *
  * @author TieuLong
  */
 public class NhanVienDAO {
-    
+
+    public static ArrayList<NhanVien> getList() {
+        try {
+            ArrayList<NhanVien> list = new ArrayList<>();
+            Connection connection = DatabaseHelper.getConnection();
+            String sql = "SELECT * FROM dbo.NhanVien WHERE TrangThai = 1";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                NhanVien nhanVien = new NhanVien();
+                nhanVien.setId(UUID.fromString(resultSet.getString("Id")));
+                nhanVien.setMataiKhoan(resultSet.getString("MataiKhoan"));
+                nhanVien.setManv(resultSet.getString("Manv"));
+                nhanVien.setTen(resultSet.getString("Ten"));
+                nhanVien.setDiachi(resultSet.getString("Diachi"));
+                nhanVien.setGioitinh(resultSet.getString("Gioitinh"));
+                nhanVien.setNgaysinh(resultSet.getDate("Ngaysinh"));
+                nhanVien.setEmail(resultSet.getString("Email"));
+                nhanVien.setSdt(resultSet.getString("Sdt"));
+                nhanVien.setLuong(resultSet.getInt("Luong"));
+                nhanVien.setNgayTao(resultSet.getDate("NgayTao"));
+                nhanVien.setNgayChinhSua(resultSet.getDate("NgayChinhSua"));
+                nhanVien.setTrangThai(resultSet.getInt("TrangThai"));
+
+                list.add(nhanVien);
+            }
+
+            connection.close();
+            return list;
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public static void insertNhanVien(NhanVien nhanVien) {
+        try {
+            Connection connection = DatabaseHelper.getConnection();
+            String sql = "INSERT INTO dbo.NHANVIEN (Id, MataiKhoan, Manv, Ten, Diachi, Gioitinh, Ngaysinh, Email, Sdt, Luong, NgayTao, NgayChinhSua, TrangThai) "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setObject(1, nhanVien.getId());
+            preparedStatement.setString(2, nhanVien.getMataiKhoan());
+            preparedStatement.setString(3, nhanVien.getManv());
+            preparedStatement.setString(4, nhanVien.getTen());
+            preparedStatement.setString(5, nhanVien.getDiachi());
+            preparedStatement.setString(6, nhanVien.getGioitinh());
+            preparedStatement.setDate(7, new java.sql.Date(nhanVien.getNgaysinh().getTime()));
+            preparedStatement.setString(8, nhanVien.getEmail());
+            preparedStatement.setString(9, nhanVien.getSdt());
+            preparedStatement.setInt(10, nhanVien.getLuong());
+            preparedStatement.setDate(11, new java.sql.Date(nhanVien.getNgayTao().getTime()));
+            preparedStatement.setDate(12, new java.sql.Date(nhanVien.getNgayChinhSua().getTime()));
+            preparedStatement.setInt(13, nhanVien.getTrangThai());
+
+            preparedStatement.executeUpdate();
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void updateNhanVien(NhanVien nhanVien) {
+        try {
+            Connection connection = DatabaseHelper.getConnection();
+            String sql = "UPDATE dbo.NHANVIEN SET MataiKhoan=?, Manv=?, Ten=?, Diachi=?, Gioitinh=?, Ngaysinh=?, Email=?, Sdt=?, Luong=?, NgayChinhSua=? WHERE Id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, nhanVien.getMataiKhoan());
+            preparedStatement.setString(2, nhanVien.getManv());
+            preparedStatement.setString(3, nhanVien.getTen());
+            preparedStatement.setString(4, nhanVien.getDiachi());
+            preparedStatement.setString(5, nhanVien.getGioitinh());
+            preparedStatement.setDate(6, new java.sql.Date(nhanVien.getNgaysinh().getTime()));
+            preparedStatement.setString(7, nhanVien.getEmail());
+            preparedStatement.setString(8, nhanVien.getSdt());
+            preparedStatement.setInt(9, nhanVien.getLuong());
+            preparedStatement.setDate(10, new java.sql.Date(nhanVien.getNgayChinhSua().getTime()));
+            preparedStatement.setObject(11, nhanVien.getId());
+
+            preparedStatement.executeUpdate();
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void deleteNhanVien(UUID id) {
+        try {
+            Connection connection = DatabaseHelper.getConnection();
+            String sql = "UPDATE dbo.NHANVIEN SET TrangThai=0 WHERE Id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setObject(1, id);
+
+            preparedStatement.executeUpdate();
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
--- src/main/java/ENTITY/NhanVien.java
+++ src/main/java/ENTITY/NhanVien.java
@@ -12,6 +12,7 @@ import java.util.UUID;
  * @author TieuLong
  */
 public class NhanVien {
+
     private UUID id;
     private String mataiKhoan;
     private String manv;
@@ -26,5 +27,132 @@ public class NhanVien {
     private Date ngayChinhSua;
     private int trangThai;
 
-    // Các phương thức getter và setter
+    public NhanVien() {
+    }
+
+    public NhanVien(UUID id, String mataiKhoan, String manv, String ten, String diachi, String gioitinh, Date ngaysinh, String email, String sdt, int luong, Date ngayTao, Date ngayChinhSua, int trangThai) {
+        this.id = id;
+        this.mataiKhoan = mataiKhoan;
+        this.manv = manv;
+        this.ten = ten;
+        this.diachi = diachi;
+        this.gioitinh = gioitinh;
+        this.ngaysinh = ngaysinh;
+        this.email = email;
+        this.sdt = sdt;
+        this.luong = luong;
+        this.ngayTao = ngayTao;
+        this.ngayChinhSua = ngayChinhSua;
+        this.trangThai = trangThai;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMataiKhoan() {
+        return mataiKhoan;
+    }
+
+    public void setMataiKhoan(String mataiKhoan) {
+        this.mataiKhoan = mataiKhoan;
+    }
+
+    public String getManv() {
+        return manv;
+    }
+
+    public void setManv(String manv) {
+        this.manv = manv;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public String getGioitinh() {
+        return gioitinh;
+    }
+
+    public void setGioitinh(String gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+
+    public Date getNgaysinh() {
+        return ngaysinh;
+    }
+
+    public void setNgaysinh(Date ngaysinh) {
+        this.ngaysinh = ngaysinh;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public int getLuong() {
+        return luong;
+    }
+
+    public void setLuong(int luong) {
+        this.luong = luong;
+    }
+
+    public Date getNgayTao() {
+        return ngayTao;
+    }
+
+    public void setNgayTao(Date ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+
+    public Date getNgayChinhSua() {
+        return ngayChinhSua;
+    }
+
+    public void setNgayChinhSua(Date ngayChinhSua) {
+        this.ngayChinhSua = ngayChinhSua;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    @Override
+    public String toString() {
+        return "NhanVien{" + "id=" + id + ", mataiKhoan=" + mataiKhoan + ", manv=" + manv + ", ten=" + ten + ", diachi=" + diachi + ", gioitinh=" + gioitinh + ", ngaysinh=" + ngaysinh + ", email=" + email + ", sdt=" + sdt + ", luong=" + luong + ", ngayTao=" + ngayTao + ", ngayChinhSua=" + ngayChinhSua + ", trangThai=" + trangThai + '}';
+    }
+
 }
