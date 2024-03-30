145046f0898e9d5b51008d29a1f0b700c7750d72
truongsilva <truongtxph35788@fpt.edu.vn>
2024-03-30 17:29:11 +0700

    ChinhSuaBanHang

--- src/main/java/DAO/BanHangDao.java
+++ src/main/java/DAO/BanHangDao.java
@@ -18,36 +18,36 @@ import java.util.List;
  */
 public class BanHangDao {
 
-    public List<ChiTietSanPham> getAll() {
-        ArrayList<ChiTietSanPham> listLh = new ArrayList<>();
+    public List<ChiTietSanPham> getAll () {
+        ArrayList<ChiTietSanPham> listLh = new ArrayList<> ();
         String sql = "SELECT CTSP.MACTSP, SP.TEN, S.TENSIZE, KD.TENKIEU, CTSP.DONGIA, CTSP.SOLUONG, CTSP.ID     \n"
                 + "FROM            CHITIETSANPHAM CTSP INNER JOIN\n"
                 + "                         KIEUDANG KD ON CTSP.Idkieu = KD.Id INNER JOIN\n"
                 + "                         SANPHAM SP ON CTSP.Idsp = SP.Id INNER JOIN\n"
                 + "                         SIZE S ON CTSP.Idsize = S.Id";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(sql)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                ChiTietSanPham ctsp = new ChiTietSanPham();
-                ctsp.setMactsp(rs.getString(1));
-                ctsp.setIdsp(rs.getString(2));
-                ctsp.setIdsize(rs.getString(3));
-                ctsp.setIdkieu(rs.getString(4));
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( sql ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                ChiTietSanPham ctsp = new ChiTietSanPham ();
+                ctsp.setMactsp ( rs.getString ( 1 ) );
+                ctsp.setIdsp ( rs.getString ( 2 ) );
+                ctsp.setIdsize ( rs.getString ( 3 ) );
+                ctsp.setIdkieu ( rs.getString ( 4 ) );
 
-                ctsp.setDongia(rs.getInt(5));
-                ctsp.setSoluong(rs.getInt(6));
-                ctsp.setId(rs.getString(7));
-                listLh.add(ctsp);
+                ctsp.setDongia ( rs.getInt ( 5 ) );
+                ctsp.setSoluong ( rs.getInt ( 6 ) );
+                ctsp.setId ( rs.getString ( 7 ) );
+                listLh.add ( ctsp );
             }
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
+        } catch ( Exception e ) {
+            e.printStackTrace ( System.out );
 
         }
         return listLh;
     }
 
-    public List<ChiTietSanPham> Tk(String ten) {
-        ArrayList<ChiTietSanPham> listLh = new ArrayList<>();
+    public List<ChiTietSanPham> Tk ( String ten ) {
+        ArrayList<ChiTietSanPham> listLh = new ArrayList<> ();
         String sql = "SELECT CTSP.MACTSP, SP.TEN, DG.DOCAODE, S.TENSIZE, KD.MAKIEU, MS.MAMAU, CTSP.DONGIA, CTSP.SOLUONG, CTSP.MOTA, CTSP.TRANGTHAI, CTSP.HINHANH, CTSP.ID     \n"
                 + "FROM            CHITIETSANPHAM CTSP INNER JOIN\n"
                 + "                         DEGIAY DG ON CTSP.Idde = DG.Id INNER JOIN\n"
@@ -55,33 +55,35 @@ public class BanHangDao {
                 + "                         MAUSAC MS ON CTSP.Idms = MS.Id INNER JOIN\n"
                 + "                         SANPHAM SP ON CTSP.Idsp = SP.Id INNER JOIN\n"
                 + "                         SIZE S ON CTSP.Idsize = S.Id Where SP.TEN =? OR CTSP.MACTSP = ?";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setObject(1, ten);
-            ps.setObject(2, ten);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                ChiTietSanPham ctsp = new ChiTietSanPham();
-                ctsp.setMactsp(rs.getString(1));
-                ctsp.setIdsp(rs.getString(2));
-                ctsp.setIdde(rs.getString(3));
-                ctsp.setIdsize(rs.getString(4));
-                ctsp.setIdkieu(rs.getString(5));
-                ctsp.setIdms(rs.getString(6));
+        try (
+                Connection con = DatabaseHelper.getConnection ();  
+                PreparedStatement ps = con.prepareStatement ( sql ) ) {
+            ps.setObject ( 1 , ten );
+            ps.setObject ( 2 , ten );
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                ChiTietSanPham ctsp = new ChiTietSanPham ();
+                ctsp.setMactsp ( rs.getString ( 1 ) );
+                ctsp.setIdsp ( rs.getString ( 2 ) );
+                ctsp.setIdde ( rs.getString ( 3 ) );
+                ctsp.setIdsize ( rs.getString ( 4 ) );
+                ctsp.setIdkieu ( rs.getString ( 5 ) );
+                ctsp.setIdms ( rs.getString ( 6 ) );
 
-                ctsp.setDongia(rs.getInt(7));
-                ctsp.setSoluong(rs.getInt(8));
-                ctsp.setId(rs.getString(9));
-                listLh.add(ctsp);
+                ctsp.setDongia ( rs.getInt ( 7 ) );
+                ctsp.setSoluong ( rs.getInt ( 8 ) );
+                ctsp.setId ( rs.getString ( 9 ) );
+                listLh.add ( ctsp );
             }
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
+        } catch ( Exception e ) {
+            e.printStackTrace ( System.out );
 
         }
         return listLh;
     }
 
-    public List<ChiTietSanPham> Loc1(String ten) {
-        ArrayList<ChiTietSanPham> listLh = new ArrayList<>();
+    public List<ChiTietSanPham> Loc1 ( String ten ) {
+        ArrayList<ChiTietSanPham> listLh = new ArrayList<> ();
         String sql = "SELECT CTSP.MACTSP, SP.TEN, CAST(S.TENSIZE AS NVARCHAR(100)) AS TENSIZE, KD.TENKIEU, CTSP.DONGIA, CTSP.SOLUONG, CTSP.ID     \n"
                 + "FROM CHITIETSANPHAM CTSP \n"
                 + "INNER JOIN DEGIAY DG ON CTSP.Idde = DG.Id \n"
@@ -90,125 +92,125 @@ public class BanHangDao {
                 + "INNER JOIN SANPHAM SP ON CTSP.Idsp = SP.Id \n"
                 + "INNER JOIN SIZE S ON CTSP.Idsize = S.Id \n"
                 + "WHERE KD.TENKIEU = ? OR CAST(S.TENSIZE AS NVARCHAR(100)) = ?";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setObject(1, ten);
-            ps.setObject(2, ten);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( sql ) ) {
+            ps.setObject ( 1 , ten );
+            ps.setObject ( 2 , ten );
 //            ps.setObject(3, ten);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                ChiTietSanPham ctsp = new ChiTietSanPham();
-                ctsp.setMactsp(rs.getString(1));
-                ctsp.setIdsp(rs.getString(2));
-                ctsp.setIdkieu(rs.getString(4));
-                ctsp.setIdsize(rs.getString(3));
-                ctsp.setIdkieu(rs.getString(4));
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                ChiTietSanPham ctsp = new ChiTietSanPham ();
+                ctsp.setMactsp ( rs.getString ( 1 ) );
+                ctsp.setIdsp ( rs.getString ( 2 ) );
+                ctsp.setIdkieu ( rs.getString ( 4 ) );
+                ctsp.setIdsize ( rs.getString ( 3 ) );
+                ctsp.setIdkieu ( rs.getString ( 4 ) );
 
-                ctsp.setDongia(rs.getInt(5));
-                ctsp.setSoluong(rs.getInt(6));
-                ctsp.setId(rs.getString(7));
-                listLh.add(ctsp);
+                ctsp.setDongia ( rs.getInt ( 5 ) );
+                ctsp.setSoluong ( rs.getInt ( 6 ) );
+                ctsp.setId ( rs.getString ( 7 ) );
+                listLh.add ( ctsp );
             }
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
+        } catch ( Exception e ) {
+            e.printStackTrace ( System.out );
 
         }
         return listLh;
     }
 
-    public List<String> getListMauSac() {
-        List<String> list = new ArrayList<>();
+    public List<String> getListMauSac () {
+        List<String> list = new ArrayList<> ();
         String query = "Select MAUSAC.TENMAU from MAUSAC ";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String str = rs.getString(1);
-                list.add(str);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( query ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                String str = rs.getString ( 1 );
+                list.add ( str );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
         }
         return list;
     }
 
-    public List<String> getListSanPham() {
-        List<String> list = new ArrayList<>();
+    public List<String> getListSanPham () {
+        List<String> list = new ArrayList<> ();
         String query = "Select SANPHAM.TEN from SANPHAM ";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String str = rs.getString(1);
-                list.add(str);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( query ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                String str = rs.getString ( 1 );
+                list.add ( str );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
         }
         return list;
     }
 
-    public List<String> getListKieuDang1() {
-        List<String> list = new ArrayList<>();
+    public List<String> getListKieuDang1 () {
+        List<String> list = new ArrayList<> ();
         String query = "select KIEUDANG.TENKIEU from KIEUDANG ";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String str = rs.getString(1);
-                list.add(str);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( query ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                String str = rs.getString ( 1 );
+                list.add ( str );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
         }
         return list;
     }
 
-    public List<String> getListSize1() {
-        List<String> list = new ArrayList<>();
+    public List<String> getListSize1 () {
+        List<String> list = new ArrayList<> ();
         String query = "select SIZE.Tensize from SIZE ";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String str = rs.getString(1);
-                list.add(str);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( query ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                String str = rs.getString ( 1 );
+                list.add ( str );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
         }
         return list;
     }
 
-    public List<String> getListChieuCao() {
-        List<String> list = new ArrayList<>();
+    public List<String> getListChieuCao () {
+        List<String> list = new ArrayList<> ();
         String query = "select DEGIAY.Docaode from DEGIAY";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String str = rs.getString(1);
-                list.add(str);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( query ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                String str = rs.getString ( 1 );
+                list.add ( str );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
         }
         return list;
     }
 
-    public List<String> getListHoaDon() {
-        List<String> list = new ArrayList<>();
+    public List<String> getListHoaDon () {
+        List<String> list = new ArrayList<> ();
         String query = "select * from HOADON";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String str = rs.getString(1);
-                list.add(str);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( query ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                String str = rs.getString ( 1 );
+                list.add ( str );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
         }
         return list;
     }
 
-    public List<String> getListHoaDonChiTiet() {
-        List<String> list = new ArrayList<>();
+    public List<String> getListHoaDonChiTiet () {
+        List<String> list = new ArrayList<> ();
         String query = "select * from HOADONCHITIET";
-        try ( Connection con = DatabaseHelper.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String str = rs.getString(1);
-                list.add(str);
+        try ( Connection con = DatabaseHelper.getConnection ();  PreparedStatement ps = con.prepareStatement ( query ) ) {
+            ResultSet rs = ps.executeQuery ();
+            while ( rs.next () ) {
+                String str = rs.getString ( 1 );
+                list.add ( str );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
         }
         return list;
     }
